3f3eb85569692dc4d94ad001a5352324c43b274b
Dron <epttit@gmail.com>
2022-07-06 13:52:57 +0300

    add task29

---
+++ task29/Program.cs
@@ -0,0 +1,30 @@
+﻿ //Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] fillArray (int[] arr)
+    {
+        int length = arr.Length;
+        int index = 0;
+        while (index < length)
+        {
+            arr[index] = new Random().Next(1, 99);
+            index++;
+        }
+        return arr;
+    }
+
+void printArray (int[] arr1)
+{
+    int count = arr1.Length;
+    int position = 0;
+    while (position < count)
+        {
+            Console.Write(arr1[position] + "|");
+            position++;
+        }
+    Console.WriteLine();
+}
+
+int[] array = new int[8];
+
+fillArray(array);
+printArray(array);
