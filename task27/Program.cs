940d1c3c2cedb117d0ce9bbbbf3f4ca696dfe69f
Dron <epttit@gmail.com>
2022-07-10 15:59:44 +0300

    add task27

---
+++ task27/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+int summery(int num)
+{
+while (a > 0)
+    {
+    sum = sum + a%10;
+    a = a/10;
+    }
+    {
+    return sum;
+    }
+}
+
+Console.WriteLine("Сумма цифр в введёном числе равняется: " + summery(a));
