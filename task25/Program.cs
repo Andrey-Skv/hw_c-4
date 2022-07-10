79830d01b981ed9d73facf25ef58bebef0a2da9f
Dron <epttit@gmail.com>
2022-07-10 15:27:27 +0300

    gg

---
+++ task25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+double Step (int x, int y)
+{
+    double Step = Math.Pow(x, y);
+    return Step;
+}
+
+
+Console.WriteLine("Число A возведённое в степень B равняется: " + Step(a, b));
